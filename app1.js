9b234712b8052fbbda37c54e846b8066f54df982
fcbyk <731240932@qq.com>
2026-05-21 10:28:50 +0800

    regex(d6): alternation

--- app1.js
+++ app1.js
@@ -1,20 +1,27 @@
 /**
-分组
-分组就是用小括号 () 把一段正则表达式包起来，当成一个整体单元使用
-把多个字符当成一个整体，给它们加量词（* + ? {n}）
+分支选择
+| （竖线）是正则里的 「或匹配」符号
+作用：满足左边 或者 右边 任意一个规则，都算匹配成功
  */
 
 const print = (...args) => args.forEach(arg => console.log(arg))
 
 
 const text = `
-cat
-catcat
-catcatcat
+catdog
+213das
+czxc
+234
 `
 
 print(
-    text.match(/(cat)+/g),           // 匹配1次或多次cat
-    text.match(/(cat){2}/g),         // 精确匹配2次cat
-    text.match(/(cat){2,3}/g),       // 匹配2-3次cat
+    /**
+     * | 的优先级非常低，
+     * 作用范围是它左右两边的整个子表达式，而不仅仅是紧邻的单个字符
+     */
+    text.match(/cat|dog/g),     //匹配 cat 或 dog
+    text.match(/cat|dog|bird|21/g),  // 可以无限叠加多个分支
+
+    // 匹配顺序：从左到右，找到即停止
+    text.match(/1|2|3|4/)
 )
--- app2.js
+++ app2.js
@@ -1,49 +1,20 @@
 /**
-捕获组 vs 非捕获组 vs 命名捕获组
-
-捕获 = 分组 + 把分组匹配到的内容保存下来
-非捕获组 = 分组 + 不把分组匹配到的内容保存下来
-命名捕获组 = 分组 + 把分组匹配到的内容保存下来并命名
+优先级问题
+| 的优先级非常低，比普通字符、量词（*+?）都低
  */
 
 const print = (...args) => args.forEach(arg => console.log(arg))
 
+
 const text = `
 cat
-catcat
-catcatcat
-catdog
+cet
+cctv
 `
 
 print(
-    // 捕获组
-    // 语法 (pattern)
-    // 默认就是捕获组
-    text.match(/(cat)(cat)/),
-    
-    // 非捕获组，无法访问分组内容
-    // 语法 (?:pattern)
-    text.match(/(?:cat){2}/),
-    
-    // 命名捕获组
-    // 语法 (?<name>pattern)
-    text.match(/(?<hello>cat)(?<x>dog)/)
+    // 用括号 () 限定范围
+    // 括号可以把分支包起来，表示「这一整块是一个选项」
+    text.match(/c(at)|(et)/g),
+    text.match(/c(a|e|c)t/g),    
 )
-
-/**
-
-text.match() 返回值 =>
-
-1. 不使用全局标志 /g：返回一个数组 
-[
-    完整匹配项（索引 0）
-    捕获组（如果有）
-    index：匹配的起始位置
-    input：原始字符串
-    groups：命名捕获组（如果有）
-]
-
-2. 使用全局标志 /g：返回所有匹配项的数组（不包含捕获组信息）
-3. 无匹配：返回 null
-
- */
