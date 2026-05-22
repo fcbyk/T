f779473e1a47608d2e818eb3a825cfbb15989dca
fcbyk <731240932@qq.com>
2026-05-22 18:14:19 +0800

    vue(d34): component events

---
+++ App.vue
@@ -0,0 +1,31 @@
+<template>
+  <p>{{ message }}</p>
+  <p>{{ password }}</p>
+
+  <!-- 绑定自定义事件 -->
+  <Child 
+    @child-click="handleChildClick" 
+    @submit-form="handleSubmit"
+  />
+</template>
+<script setup>
+import Child from './Child.vue'
+import { ref } from 'vue'
+
+const message = ref('')
+const password = ref({})
+
+/**
+ * 父组件定义回调函数
+ * 由 子组件 触发自定义事件 调用
+ */
+const handleChildClick = (msg) => {
+  console.log('父组件收到：', msg)
+  message.value = msg
+}
+
+const handleSubmit = (formData) => {
+  console.log('表单数据：', formData)
+  password.value = formData.password
+}
+</script>
---
+++ Child.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="handleClick">点击重新传值给父组件</button>
+</template>
+
+<script setup>
+
+// 声明自定义事件（数组/对象语法）
+const emit = defineEmits(['child-click', 'submit-form'])
+
+let count = 0
+let form = {
+  username: 'vue',
+  password: 1000
+}
+
+// 直接触发自定义事件：emit(事件名, 传递的参数)
+emit('child-click', `来自子组件的消息${count}`)
+emit('submit-form', form)
+
+/**
+ * 间接触发自定义事件
+ * @description 子组件点击事件 -> 触发自定义事件
+ */
+const handleClick = () => {
+  count+=1
+  form.password+=1
+  emit('child-click', `来自子组件的消息${count}`)
+  emit('submit-form', form)
+}
+</script>
