767bf46a07dd2fda8e68c32223d552eebdfdf638
fcbyk <731240932@qq.com>
2026-05-22 17:07:36 +0800

    vue(d29): computed properties and watchers

--- App1.vue
+++ App1.vue
@@ -1,47 +1,69 @@
-<!-- 内置指令 -->
-<!-- https://cn.vuejs.org/api/built-in-directives.html#v-text -->
-
+<!-- 
+计算属性: 计算属性是基于它们的依赖进行缓存的，只有在它的相关依赖发生改变时才会重新求值
+-->
 <template>
-  
-  <!-- v-text -->
-  <div>{{ message }}</div>
-  <!-- 等同于 -->
-  <div v-text="message"></div>
-
   <!-- 
-  v-html 
-  v-html 的内容直接作为普通 HTML 插入
-  -->
-  <div v-html="html"></div>
+   三者区别查看官方文档 
+   https://cn.vuejs.org/guide/essentials/computed.html
+   -->
 
-  <!-- 
-  v-pre
-  跳过该元素及其所有子元素的编译
-  元素内具有 v-pre，所有 Vue 模板语法都会被保留并按原样渲染
-  -->
-  <div v-pre>{{ message }}</div>
+  <!-- 不使用计算属性 -->
+  <p>Has published books:</p>
+  <span>{{ author.books.length > 0 ? 'Yes' : 'No' }}</span>
 
-  <!-- 
-  v-once
-  只渲染元素和组件一次，之后的修改不会重新渲染
-  -->
-  <div>
-    <button @click="message = 'Hello Vue'">Click</button>
-  </div>
-  <div v-once>{{ message }}</div>
+  <!-- 使用计算属性 -->
+  <p>Has published books:</p>
+  <span>{{ publishedBooksMessage }}</span>
 
-  <!-- 
-  v-cloak
-  用于防止在 Vue 实例初始化之前或之后闪现未编译的 Mustache 标签
-  直到编译完成前，<div> 将不可见
-  -->
-  <div v-cloak>{{ message }}</div>
+  <!-- 使用普通函数 -->
+  <p>Has published books:</p>
+  <p>{{ calculateBooksMessage() }}</p>
 
+
+  <!-- 使用可写计算属性 -->
+  <p>first name: {{firstName }}</p>
+  <p>last name: {{ lastName }}</p>
+  <p>full name: {{ fullName }}</p>
+  <input v-model="fullName" />
 </template>
 
-
 <script setup>
-  import { ref } from 'vue'
-  const message = ref('Hello World')
-  const html = '<span style="color: red;">Hello World</span>'
+import { reactive, computed, ref } from 'vue'
+
+const author = reactive({
+  name: 'John Doe',
+  books: [
+    'Vue 2 - Advanced Guide',
+    'Vue 3 - Basic Guide',
+    'Vue 4 - The Mystery'
+  ]
+})
+
+// 定义计算属性
+const publishedBooksMessage = computed(() => {
+  return author.books.length > 0 ? 'Yes' : 'No'
+})
+
+// 定义一个普通函数
+function calculateBooksMessage() {
+  return author.books.length > 0 ? 'Yes' : 'No'
+}
+
+
+// 可写计算属性
+const firstName = ref('John')
+const lastName = ref('Doe')
+
+const fullName = computed({
+  // getter
+  get() {
+    return firstName.value + ' ' + lastName.value
+  },
+  // setter
+  set(newValue) {
+    // 使用的是解构赋值语法
+    [firstName.value, lastName.value] = newValue.split(' ')
+  }
+})
+
 </script>
