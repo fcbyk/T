98cdc8a23dbf32c1cf0b7257e511545abb2e430b
fcbyk <731240932@qq.com>
2026-05-22 17:22:25 +0800

    vue(d31): component registration, props, global registration, local registration

--- app3.mjs
+++
@@ -1,5 +0,0 @@
-import { createApp } from "vue"
-import App3 from "./sfc/App3.vue"
-
-// 使用SFC,代替组件的配置对象的写法（单文件组件）（选项式API）
-createApp(App3).mount('#app')
--- app5.mjs
+++
@@ -1,5 +0,0 @@
-import { createApp } from "vue"
-import App5 from "./sfc/App5.vue"
-
-// SFC 组合式API (<script setup> 语法糖)
-createApp(App5).mount('#app')
