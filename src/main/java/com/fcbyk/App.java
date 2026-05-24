f863fae94bd850e5776b3e15beb02be3d4573b6c
fcbyk <731240932@qq.com>
2026-05-24 15:41:30 +0800

    spring(d73): Combining annotations and XML usage Component (Controller, Service, Repository)

--- src/main/java/com/fcbyk/App.java
+++ src/main/java/com/fcbyk/App.java
@@ -1,7 +1,5 @@
 package com.fcbyk;
 
-import com.alibaba.druid.pool.DruidDataSource;
-import javax.sql.DataSource;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,17 +8,8 @@ public class App {
     // 管理第三方bean
     public static void main(String arg[]){
         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-        // 获取DruidDataSource对象
-        DruidDataSource dataSource = (DruidDataSource) ctx.getBean("dataSource");
         
-        System.out.println("===== Druid 连接池配置信息 =====");
-        System.out.println("驱动类: " + dataSource.getDriverClassName());
-        System.out.println("URL: " + dataSource.getUrl());
-        System.out.println("用户名: " + dataSource.getUsername());
-        System.out.println("初始连接数: " + dataSource.getInitialSize());
-        System.out.println("最大活跃连接数: " + dataSource.getMaxActive());
-        System.out.println("最小空闲连接数: " + dataSource.getMinIdle());
-        System.out.println("\n===== Druid 运行时状态 =====");
-        System.out.println(dataSource);
+        Student student = (Student) ctx.getBean("student");
+        student.test();
     }
 }
