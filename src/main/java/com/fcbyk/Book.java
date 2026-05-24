643b875088779fc893631d6b1e05d478ec61d865
fcbyk <731240932@qq.com>
2026-05-24 15:29:28 +0800

    spring(d71): setter injection, constructor injection, autowiring, collection injection

--- src/main/java/com/fcbyk/Book.java
+++ src/main/java/com/fcbyk/Book.java
@@ -12,13 +12,4 @@ public class Book {
         System.out.println("图书test方法执行");
     }
 
-    // 生命周期方法，表示bean初始化对应的操作
-    public void init(){
-        System.out.println("图书bean初始化");
-    }
-
-    // 生命周期方法，表示bean销毁前对应的操作
-    public void destroy(){
-        System.out.println("图书bean销毁");
-    }
 }
--- src/main/java/com/fcbyk/MyOrder.java
+++ src/main/java/com/fcbyk/MyOrder.java
@@ -1,28 +1,13 @@
 package com.fcbyk;
 
-// 如果不在xml配置生命周期对应的函数，也可以通过类实现InitializingBean与DisposableBean接口
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class MyOrder implements InitializingBean, DisposableBean {
+public class MyOrder {
 
     public MyOrder() {
-        System.out.println("1 订单对象已被创建");
+        System.out.println("订单对象已被创建");
     }
 
     public void test() {
         System.out.println("测试方法执行");
     }
 
-    // Spring容器关闭时执行，Bean销毁
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("3 订单bean销毁");
-    }
-
-    // 属性注入完成后执行，Bean初始化
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("2 订单bean初始化");
-    }
 }
